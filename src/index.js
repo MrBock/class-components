dbf2cd751e6d2e3b64d7def1e7f7d7bcd288bbcc
Mr_Bock <26689948+MrBock@users.noreply.github.com>
2021-06-14 22:03:13 +0200

    Create a class component using a constructur and componentDidMount() to load data.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  // React.component already has it's own constructor. With super(props) we're making sure that the parenting constructor still gets called.
+  constructor(props) {
+    super(props);
+    // defaulting to null because we don't know the latitude value yet; this is the only time to directly assign to this.state
+    this.state = { lat: null, errorMessage: '' };
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //the react documentation states componentDidMount() as the best place to load data
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+  // React requires us to define render()
+  render() {
+    // the following is referred to as conditional rendering. we're returning different JSX each time the component is rerendered based on the state update
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
